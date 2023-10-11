f87b0eb8f4942efab89bf92f67f105fa5e6c75f6
Nazim8115 <nazim.nn459@gmail.com>
2023-10-11 23:45:44 +0530

    findIndex

---
+++ Array/FindIndex.java
@@ -0,0 +1,19 @@
+/**
+ * FindIndex
+ */
+public class FindIndex {
+    public static int findIndex(int[] arr, int x) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 10, 20, 40, 30, 50, 90 };
+        int res = findIndex(arr, 30);
+        System.out.println(res);
+    }
+}
