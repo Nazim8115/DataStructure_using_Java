7aa154c9654fc04380d902d098025870637180b5
Nazim8115 <nazim.nn459@gmail.com>
2023-10-15 22:18:48 +0530

    add isvalue present

---
+++ Array/FindValue.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class FindValue {
+    public static boolean isValue(int arr[], int x) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("enter value : ");
+        int value = sc.nextInt();
+        System.out.println(isValue(arr, value));
+    }
+}
