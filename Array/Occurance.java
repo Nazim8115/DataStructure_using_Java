d09f13ddeb138e2589cf6dc6ec4b6eab19211f55
Nazim8115 <nazim.nn459@gmail.com>
2023-10-21 19:17:51 +0530

    occurance using map

--- Array/Occurance.java
+++ Array/Occurance.java
@@ -1,6 +1,28 @@
 import java.util.*;
 
 public class Occurance {
+    static void countFreq3(int arr[], int n) {
+        Map<Integer, Integer> mp = new HashMap<>();
+
+        // Traverse through array elements and
+        // count frequencies
+        for (int i = 0; i < n; i++) {
+            mp.put(arr[i], mp.get(arr[i]) == null ? 1 : mp.get(arr[i]) + 1);
+        }
+
+        // To print elements according to first
+        // occurrence, traverse array one more time
+        // print frequencies of elements and mark
+        // frequencies as -1 so that same element
+        // is not printed multiple times.
+        for (int i = 0; i < n; i++) {
+            if (mp.get(arr[i]) != -1) {
+                System.out.println(arr[i] + " " + mp.get(arr[i]));
+                mp.put(arr[i], -1);
+            }
+        }
+    }
+
     static void countFreq2(int arr[], int n) {
         Map<Integer, Integer> mp = new HashMap<>();
 
@@ -46,7 +68,7 @@ public class Occurance {
 
     public static void main(String[] args) {
         int arr[] = { 1, 2, 22, 1, 90, 80, 60 };
-        countFreq2(arr, arr.length);
+        countFreq3(arr, arr.length);
 
     }
 
