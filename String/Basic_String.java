769db90390f6863b3a04eae2a032fee7788ba697
Nazim8115 <nazim.nn459@gmail.com>
2023-11-04 19:53:33 +0530

    learn string in java

---
+++ String/Basic_String.java
@@ -0,0 +1,12 @@
+package String;
+
+public class Basic_String {
+    public static void main(String[] args) {
+        // A string is a sequence of characters. for example "hello" is a string
+        // containing sequence of characters 'h','e','l','l','o' we have double quotes
+        // to represent string in java...for exampls...
+        String s = "java programing";
+        System.out.println(s);
+
+    }
+}
