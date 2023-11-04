241f0f5fb3da36143f35179bac8906c928c54fb3
Nazim8115 <nazim.nn459@gmail.com>
2023-11-04 20:26:19 +0530

    understand userinput in the tring and different between next() and nextline()

--- String/Basic_String.java
+++ String/Basic_String.java
@@ -1,24 +1,45 @@
 package String;
 
+import java.util.Scanner;
+
 public class Basic_String {
     public static void main(String[] args) {
-        // A string is a sequence of characters. for example "hello" is a string
-        // containing sequence of characters 'h','e','l','l','o' we have double quotes
-        // to represent string in java...for exampls...
-        String s = "java programing";
-        System.out.println(s);
+        /*
+         * A string is a sequence of characters. for example "hello" is a string
+         * containing sequence of characters 'h','e','l','l','o' we have double quotes
+         * to represent string in java...for exampls...
+         */
+
+        // String s = "java programing";
+        // System.out.println(s);
 
         // methods of string
         // find length of the string
-        int le = s.length();
-        System.out.println(le);
+        // int le = s.length();
+        // System.out.println(le);
         // find length witout lenth dunction
 
-        int l = 0;
+        // int l = 0;
 
-        for (char c : s.toCharArray()) {
-            l++;
-        }
+        // for (char c : s.toCharArray()) {
+        // l++;
+        // }
+
+        /* More about string input by use and other inbuilt methods */
+        Scanner s = new Scanner(System.in);
+        // String userinput = s.next();
+        // System.out.println(userinput);
+        // next() method in java used for reading the string ! next() can read the input
+        // only till the space. for example my input is my name but its read only my
+
+        /*
+         * Nextline() reads input includig space between the words till end for example
+         * input strean -- my name is nazim khan =>(its read till end
+         * ) my name is nazim khan
+         */
+
+        String userFullName = s.nextLine();
+        System.out.println(userFullName);
 
     }
 }
