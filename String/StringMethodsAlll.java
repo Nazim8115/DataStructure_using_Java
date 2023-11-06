cbe3b8b3efc020c2bcf36319dec32e55a5b52bbd
Nazim8115 <nazim.nn459@gmail.com>
2023-11-06 21:23:10 +0530

    inbuilt methods seprate file

---
+++ String/StringMethodsAlll.java
@@ -0,0 +1,14 @@
+package String;
+
+import java.util.Scanner;
+
+public class StringMethodsAlll {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println("Original string : " + str);
+        // charAt()
+        System.out.println(str.charAt(2));
+
+    }
+}
