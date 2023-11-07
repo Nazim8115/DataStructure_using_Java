fd9333f8eba91a78bf4f73fe761617205a418606
Nazim8115 <nazim.nn459@gmail.com>
2023-11-07 21:27:02 +0530

    add more methods like codepointAt codepointcount and charat etc

--- String/StringMethodsAlll.java
+++ String/StringMethodsAlll.java
@@ -6,9 +6,21 @@ public class StringMethodsAlll {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
-        System.out.println("Original string : " + str);
+        // System.out.println("Original string : " + str);
         // charAt()
-        System.out.println(str.charAt(2));
+        // System.out.println(str.charAt(2));
+
+        // codePointAt() => Returns the unicode of the character at the specified index
+        // String alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        // System.out.println(str.codePointBefore(1));
+        // for (int i = 0; i < alphabets.length(); i++) {
+        // System.out.println(alphabets.codePointAt(i));
+        // }
+        // codePointCount() => Returns the number of Unicode values found in string
+        // System.out.println(str.codePointCount(0, 6));
+
+        sc.close();
 
     }
 }
